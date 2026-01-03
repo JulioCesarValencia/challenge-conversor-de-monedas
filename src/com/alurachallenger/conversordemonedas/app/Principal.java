ed00b475cb843b928008f4926fc0cdf78c0aeeb6
Julio Cesar Valencia <sesarisuma@gmail.com.com>
2026-01-03 01:13:29 -0500

    Refactorización de la clase Principal: - Eliminación de código repetido usando método convertirMoneda(). - Personalización de mensajes de error al ingresar caracteres inválidos. - Simplificación del switch del menú y manejo de excepciones.

--- src/com/alurachallenger/conversordemonedas/app/Principal.java
+++ src/com/alurachallenger/conversordemonedas/app/Principal.java
@@ -4,6 +4,7 @@ package com.alurachallenger.conversordemonedas.app;
 import com.alurachallenger.conversordemonedas.servicios.ConsultaCambioMoneda;
 import com.alurachallenger.conversordemonedas.servicios.ConvertidorMoneda;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Principal {
@@ -12,24 +13,23 @@ public class Principal {
         int opcion = 0;
         System.out.println("\n*******************Bienvenido al Conversor de Monedas****************************************\n");
 
-        ConsultaCambioMoneda consulta = new ConsultaCambioMoneda();
-
         String menu = """
-                
-                        * 1. Dólar (USD) =>> Peso Colombiano (COP)   *
-                        * 2. Peso Colombiano (COP) =>> Dólar (USD)   *
-                        * 3. Dólar (USD) =>> Real Brasileño (BRL)    *
-                        * 4. Real Brasileño (BRL) =>> Dólar (USD)    *
-                        * 5. Dólar (USD) =>> Peso Mexicano (MXN)     *
-                        * 6. Peso Mexicano (MXN) =>> Dólar (USD)     *
-                        * 7. Euro (EUR) =>> Yen Japonés (JPY)        *
-                        * 8. Libra Esterlina (GBP) =>> Yuan Chino (CNY) *
-                        * 9. Salir                             *
-                        ****************************************
-                        Elija una opción válida:""";
+                                
+                * 1. Dólar (USD) =>> Peso Colombiano (COP)   *
+                * 2. Peso Colombiano (COP) =>> Dólar (USD)   *
+                * 3. Dólar (USD) =>> Real Brasileño (BRL)    *
+                * 4. Real Brasileño (BRL) =>> Dólar (USD)    *
+                * 5. Dólar (USD) =>> Peso Mexicano (MXN)     *
+                * 6. Peso Mexicano (MXN) =>> Dólar (USD)     *
+                * 7. Euro (EUR) =>> Yen Japonés (JPY)        *
+                * 8. Libra Esterlina (GBP) =>> Yuan Chino (CNY) *
+                * 9. Salir                             *
+                ****************************************
+                Elija una opción válida:""";
 
+
+        ConsultaCambioMoneda consulta = new ConsultaCambioMoneda();
         Scanner lectura = new Scanner(System.in);
-
 
         while (opcion != 9) {
             System.out.println(menu);
@@ -44,137 +44,47 @@ public class Principal {
 
 
                 switch (opcion) {
-                    case 1 -> {
-                        try {
-                            System.out.print("Ingrese el valor que desea convertir: ");
-                            double cantidad = lectura.nextDouble();
-
-                            var respuesta = consulta.obtenerCambio("USD");
-                            var convertidor = new ConvertidorMoneda(respuesta.rates());
-                            double resultado = convertidor.convertir("COP", cantidad);
-
-                            System.out.println("El valor " + cantidad + " USD corresponde al valor final =>>> " + resultado + " COP");
-                        } catch (Exception e) {
-                            System.out.println("Error: " + e.getMessage());
-                            lectura.nextLine(); // limpiar buffer
-                        }
-                    }
-                    case 2-> {
-                        try {
-                            System.out.print("Ingrese el valor que desea convertir: ");
-                            double cantidad = lectura.nextDouble();
-
-                            var respuesta = consulta.obtenerCambio("COP");
-                            var convertidor = new ConvertidorMoneda(respuesta.rates());
-                            double resultado = convertidor.convertir("USD", cantidad);
-
-                            System.out.println(cantidad + " COP = " + resultado + " USD");
-                        } catch (Exception e) {
-                            System.out.println("Error: " + e.getMessage());
-                            lectura.nextLine(); // limpiar buffer
-                        }
-                    }
-                    case 3-> {
-                        try {
-                            System.out.print("Ingrese el valor que desea convertir: ");
-                            double cantidad = lectura.nextDouble();
-
-                            var respuesta = consulta.obtenerCambio("USD");
-                            var convertidor = new ConvertidorMoneda(respuesta.rates());
-                            double resultado = convertidor.convertir("BRL", cantidad);
-
-                            System.out.println(cantidad + " USD = " + resultado + " BRL");
-                        } catch (Exception e) {
-                            System.out.println("Error: " + e.getMessage());
-                            lectura.nextLine(); // limpiar buffer
-                        }
-                    }
-                    case 4-> {
-                        try {
-                            System.out.print("Ingrese el valor que desea convertir: ");
-                            double cantidad = lectura.nextDouble();
-
-                            var respuesta = consulta.obtenerCambio("BRL");
-                            var convertidor = new ConvertidorMoneda(respuesta.rates());
-                            double resultado = convertidor.convertir("USD", cantidad);
-
-                            System.out.println(cantidad + " BRL = " + resultado + " USD");
-                        } catch (Exception e) {
-                            System.out.println("Error: " + e.getMessage());
-                            lectura.nextLine(); // limpiar buffer
-                        }
-                    }
-                    case 5-> {
-                        try {
-                            System.out.print("Ingrese el valor que desea convertir: ");
-                            double cantidad = lectura.nextDouble();
-
-                            var respuesta = consulta.obtenerCambio("USD");
-                            var convertidor = new ConvertidorMoneda(respuesta.rates());
-                            double resultado = convertidor.convertir("MXN", cantidad);
-
-                            System.out.println(cantidad + " USD = " + resultado + " MXN");
-                        } catch (Exception e) {
-                            System.out.println("Error: " + e.getMessage());
-                            lectura.nextLine(); // limpiar buffer
-                        }
-                    }
-                    case 6-> {
-                        try {
-                            System.out.print("Ingrese el valor que desea convertir: ");
-                            double cantidad = lectura.nextDouble();
-
-                            var respuesta = consulta.obtenerCambio("MXN");
-                            var convertidor = new ConvertidorMoneda(respuesta.rates());
-                            double resultado = convertidor.convertir("USD", cantidad);
-
-                            System.out.println(cantidad + " MXN = " + resultado + " USD");
-                        } catch (Exception e) {
-                            System.out.println("Error: " + e.getMessage());
-                            lectura.nextLine(); // limpiar buffer
-                        }
-                    }
-                    case 7-> {
-                        try {
-                            System.out.print("Ingrese el valor que desea convertir: ");
-                            double cantidad = lectura.nextDouble();
-
-                            var respuesta = consulta.obtenerCambio("EUR");
-                            var convertidor = new ConvertidorMoneda(respuesta.rates());
-                            double resultado = convertidor.convertir("JPY", cantidad);
-
-                            System.out.println(cantidad + " EUR = " + resultado + " JPY");
-                        } catch (Exception e) {
-                            System.out.println("Error: " + e.getMessage());
-                            lectura.nextLine(); // limpiar buffer
-                        }
-                    }
-                        case 8-> {
-                            try {
-                                System.out.print("Ingrese el valor que desea convertir: ");
-                                double cantidad = lectura.nextDouble();
-
-                                var respuesta = consulta.obtenerCambio("GBP");
-                                var convertidor = new ConvertidorMoneda(respuesta.rates());
-                                double resultado = convertidor.convertir("CNY", cantidad);
-
-                                System.out.println(cantidad + " GBP = " + resultado + " CNY");
-                            } catch (Exception e) {
-                                System.out.println("Error: " + e.getMessage());
-                                lectura.nextLine(); // limpiar buffer
-                            }
-                        }
-
+                    case 1 -> convertirMoneda(consulta, lectura, "USD", "COP");
+                    case 2 -> convertirMoneda(consulta, lectura, "COP", "USD");
+                    case 3 -> convertirMoneda(consulta, lectura, "USD", "BRL");
+                    case 4 -> convertirMoneda(consulta, lectura, "BRL", "USD");
+                    case 5 -> convertirMoneda(consulta, lectura, "USD", "MXN");
+                    case 6 -> convertirMoneda(consulta, lectura, "MXN", "USD");
+                    case 7 -> convertirMoneda(consulta, lectura, "EUR", "JPY");
+                    case 8 -> convertirMoneda(consulta, lectura, "GBP", "CNY");
+                    default -> System.out.println("Opción no válida");
                 }
 
+
+
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada inválida. Debe ingresar un número valido.");
+                lectura.nextLine();
             } catch (Exception e) {
-                // Capturo cualquier error de lectura o convercion
-                System.out.println("Error: " + e.getMessage());
-                lectura.nextLine(); // Limpio buffer en caso de error de entrada
+                System.out.println("Ocurrió un error inesperado.");
             }
         }
     }
 
+    private static void convertirMoneda (ConsultaCambioMoneda consulta, Scanner lectura, String monedaBase, String monedaDestino) {
+        try {
+            System.out.print("Ingrese el valor que desea convertir: ");
+            double cantidad = lectura.nextDouble();
 
+            var respuesta = consulta.obtenerCambio(monedaBase);
+            var convertidor = new ConvertidorMoneda(respuesta.rates());
+            double resultado = convertidor.convertir(monedaDestino, cantidad);
 
+            System.out.println(
+                    "El valor " + cantidad + " " + monedaBase +
+                            " corresponde al valor final =>>> " +
+                            resultado + " " + monedaDestino
+            );
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            lectura.nextLine();
+        }
+    }
 }
+
